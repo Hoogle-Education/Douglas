31dee365a6beb498d43ac3b15669d0b52eb566c5
hgrafa <hugorafell@gmail.com>
2022-10-06 15:54:36 -0300

    Adicionando: Exemplo Flípper

---
+++ samples/Fliper/src/Flipper.java
@@ -0,0 +1,17 @@
+public class Flipper {
+
+	public static void main(String[] args) {
+
+		int p = 0;
+		int r = 0;
+
+		if (p == 0)
+			System.out.println("C");
+		else if (r == 0)
+			System.out.println("B");
+		else
+			System.out.println("A");
+		
+	}
+
+}
